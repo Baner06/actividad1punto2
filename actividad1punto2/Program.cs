4f17f868af9b08af8304bcfe8157c7c2acfc1f3f
Benigno <Benigno@LAPTOP-L79Q032D>
2021-08-25 14:43:07 -0500

    Agregar archivos de proyecto.

---
+++ actividad1punto2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace actividad1punto2
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			double pulsacionesPorSegundo;
+			int genero, edad;
+			Console.WriteLine("POR FAVOR INGRESE LOS DATOS SOLICITADOS:\n");
+			Console.WriteLine("Digite Su Edad:");
+			edad = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Seleccione el Genero\n1 Femenino\n2 Masculino");
+			genero = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("\nRESULTADOS:\n");
+			if (genero == 1)
+			{
+				pulsacionesPorSegundo = (Double)(220 - edad) / 10;
+				Console.WriteLine("El número de pulsaciones que usted debe tener es: " + pulsacionesPorSegundo + " Por cada 10 Segundos.");
+			}
+			else if (genero == 2)
+			{
+				pulsacionesPorSegundo = (Double)(210 - edad) / 10;
+				Console.WriteLine("El número de pulsaciones que usted debe tener es: " + pulsacionesPorSegundo + " Por cada 10 Segundos.");
+			}
+			else
+			{
+				Console.WriteLine("Error en los datos ingresados.");
+			}
+
+		}
+	}
+}
